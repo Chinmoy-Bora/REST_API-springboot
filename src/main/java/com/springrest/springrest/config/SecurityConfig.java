899c7750d399cb9da78beda734136716bd8d30f8
Chinmoy Bora <chinmoyborah100@gmail.com>
2023-10-16 12:19:04 +0530

    added the security configuration

---
+++ src/main/java/com/springrest/springrest/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.springrest.springrest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+	
+	@Bean
+	
+	public UserDetailsService userDetailsService(PasswordEncoder encoder)
+	{
+	    UserDetails admin= User.withUsername("Chinmoy")
+	    		.password(encoder.encode("Pwd1"))
+	    		.roles("ADMIN")
+	    		.build();
+	    return new InMemoryUserDetailsManager(admin);
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//            .authorizeRequests()
+//            .antMatchers("/public/**").permitAll()
+//            .anyRequest().authenticated()
+//            .and()
+//            .httpBasic();
+//    }
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		return http.csrf().disable()
+				.authorizeRequests()
+				.requestMatchers().permitAll()
+				.and()
+				.authorizeRequests().regexMatchers("/user/**")
+				.authenticated()
+				.and().formLogin()
+				.and().build();
+		
+
+		
+	}
+	
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+
